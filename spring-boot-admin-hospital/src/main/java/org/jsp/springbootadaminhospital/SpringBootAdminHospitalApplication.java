e873b36c99977cdc71b27f9cb0c511bf8b9c4c6f
shashibhushan <bshashi8083@gmail.com>
2024-03-01 14:51:52 +0530

    Added successfully

---
+++ spring-boot-admin-hospital/src/main/java/org/jsp/springbootadaminhospital/SpringBootAdminHospitalApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.springbootadaminhospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminHospitalApplication.class, args);
+	}
+
+}
---
+++ spring-boot-admin-hospital/src/test/java/org/jsp/springbootadaminhospital/SpringBootAdminHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.springbootadaminhospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
